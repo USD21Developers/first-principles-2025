b06a49fa3afe0d49dd440b5933018eb70d2e9f1f
Jason McNeill <vrtjason@gmail.com>
2025-08-20 12:16:46 -0700

    Cache CDN-hosted assets

--- workbox-config.js
+++ workbox-config.js
@@ -4,4 +4,42 @@ module.exports = {
   globIgnores: ["_assets/img/screenshots/**"],
   swDest: "sw.js",
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+  runtimeCaching: [
+    {
+      urlPattern:
+        /^https:\/\/cdn\.jsdelivr\.net\/npm\/bootstrap@5\.3\.7\/dist\/css\/bootstrap\.min\.css$/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "bootstrap-css",
+        expiration: {
+          maxEntries: 1,
+          maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
+        },
+      },
+    },
+    {
+      urlPattern:
+        /^https:\/\/cdn\.jsdelivr\.net\/npm\/bootstrap@5\.3\.7\/dist\/js\/bootstrap\.bundle\.min\.js$/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "bootstrap-js",
+        expiration: {
+          maxEntries: 1,
+          maxAgeSeconds: 60 * 60 * 24 * 30,
+        },
+      },
+    },
+    {
+      urlPattern:
+        /^https:\/\/cdn\.jsdelivr\.net\/npm\/@popperjs\/core@2\.11\.8\/dist\/umd\/popper\.min\.js$/,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "popper-js",
+        expiration: {
+          maxEntries: 1,
+          maxAgeSeconds: 60 * 60 * 24 * 30,
+        },
+      },
+    },
+  ],
 };
