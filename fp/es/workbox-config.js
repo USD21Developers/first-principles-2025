75ba79f729849727f1c6114b3f89cca60d40f71b
Jason McNeill <vrtjason@gmail.com>
2025-09-01 15:23:35 -0700

    Separate languages into their own directories

---
+++ fp/es/workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  globDirectory: ".",
+  globPatterns: ["**/*.{css,jpg,svg,png,js,avif,html,ico,webp,json,md}"],
+  globIgnores: ["_assets/img/screenshots/**", "**/ai.txt"],
+  swDest: "sw.js",
+  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+};
