d448e2dfd8a3fc85ad6be069826a8786edf194c1
Jason McNeill <vrtjason@gmail.com>
2025-09-02 11:51:59 -0700

    Restore FP-specific launch icons to the directory structure

--- fp/en/workbox-config.js
+++ fp/en/workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   globDirectory: ".",
   globPatterns: ["**/*.{css,jpg,svg,png,js,avif,html,ico,webp,json,md}"],
-  globIgnores: ["_assets/img/screenshots/**", "**/ai.txt"],
+  globIgnores: ["_assets/img/screenshots/**", "**/ai.txt", "_assets/img/icons/launch/fp/**"],
   swDest: "sw.js",
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
 };
