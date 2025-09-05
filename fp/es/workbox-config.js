60de431b02c69874419fb9b235c1d42f2d787526
Jason McNeill <vrtjason@gmail.com>
2025-09-05 14:24:02 -0700

    Add node_modules to globIgnores

--- fp/es/workbox-config.js
+++ fp/es/workbox-config.js
@@ -1,7 +1,12 @@
 module.exports = {
   globDirectory: ".",
   globPatterns: ["**/*.{css,jpg,svg,png,js,avif,html,ico,webp,json,md}"],
-  globIgnores: ["_assets/img/screenshots/**", "**/ai.txt", "_assets/img/icons/launch/fp/**"],
+  globIgnores: [
+    "_assets/img/screenshots/**",
+    "**/ai.txt",
+    "_assets/img/icons/launch/fp/**",
+    "node_modules/**",
+  ],
   swDest: "sw.js",
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
 };
