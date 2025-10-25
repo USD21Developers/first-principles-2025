d90086e9e923a22f34e8865ace447d4abcf07ef9
Jason McNeill <vrtjason@gmail.com>
2025-10-24 18:20:52 -0700

    Add Chinese

--- fp/workbox-config.js
+++ fp/workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   globDirectory: ".",
   globPatterns: ["**/*.{css,jpg,svg,png,js,avif,html,ico,webp,json,md}"],
-  globIgnores: ["node_modules/**", "en/**", "es/**", "fr/**", "pt/**"],
+  globIgnores: ["node_modules/**", "en/**", "es/**", "fr/**", "pt/**", "zh/**"],
   swDest: "sw.js",
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
   skipWaiting: true,
