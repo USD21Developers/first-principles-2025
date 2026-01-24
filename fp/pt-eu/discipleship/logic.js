75aa561152c6bb73011afb9254491da167afb262
Jason McNeill <vrtjason@gmail.com>
2026-01-24 14:05:37 -0700

    Initial commit for PT-EU translation

---
+++ fp/pt-eu/discipleship/logic.js
@@ -0,0 +1,31 @@
+function localizeNumbers() {
+  const userLocale = navigator.language || "en-US";
+  document
+    .querySelector("tbody.numerals")
+    .querySelectorAll("td")
+    .forEach((el) => {
+      if (isNaN(el.innerText)) {
+        return;
+      }
+
+      const num = parseFloat(el.innerText);
+      const formatted = new Intl.NumberFormat(userLocale).format(num);
+      el.textContent = formatted;
+    });
+}
+
+async function init() {
+  await translate();
+  linkifyScriptures();
+  shareLink();
+  localizeNumbers();
+  hideSpinner();
+}
+
+init();
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("../sw.js").catch((error) => {
+    console.error("Error in registering service worker:", error);
+  });
+}
